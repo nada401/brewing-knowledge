fb9d2c1b90b2c77565a61d3c64b074a49d7b0376
manuelcurnis <manuel.curnis@epfl.ch>
2024-11-10 22:36:02 +0100

    data cleaning updates

---
+++ src/scripts/data_cleaning/.ipynb_checkpoints/data_cleaning-checkpoint.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+import pandas as pd
+def format_attribute_ratings(ratings):
+   
+    '''
+    formatted  all attributes of ratings dataframe
+    '''
+    ratings['user_id'] = ratings['user_id'].astype(str)
+    ratings['rating'] = ratings['rating'].astype(float)
+    ratings['abv'] = ratings['abv'].astype(float)
+    ratings['appearance'] = ratings['appearance'].astype(float)
+    ratings['aroma'] = ratings['aroma'].astype(float)
+    ratings['palate'] = ratings['palate'].astype(float)
+    ratings['taste'] = ratings['taste'].astype(float)
+    ratings['overall'] = ratings['overall'].astype(float)
+    ratings['date'] = ratings['date'].astype(int)
+    ratings['date'] = ratings['date'].apply(lambda data_seconds : datetime.fromtimestamp(data_seconds).date())
+    
+    return ratings
+    
+def format_attribute_beers(beers):
+   
+    '''
+    formatted  all attributes of beers dataframe
+    '''
+
+    beers['nbr_ratings'] = beers['nbr_ratings'].astype(int)
+    beers['overall_score'] = beers['overall_score'].astype(float)
+    beers['style_score'] = beers['style_score'].astype(float)
+    beers['avg'] = beers['avg'].astype(float)
+    beers['abv'] = beers['abv'].astype(float)
+    beers['avg_computed']  = beers['avg_computed'].astype(float)
+    beers['zscore'] = beers['zscore'].astype(float)
+    beers['nbr_matched_valid_ratings'] = beers['nbr_matched_valid_ratings'].astype(int)
+    beers['avg_matched_valid_ratings'] = beers['avg_matched_valid_ratings'].astype(float)
+    
+    return beers
+
+def format_attribute_beers_BA(beers):
+   
+    '''
+    formatted  all attributes of beers dataframe
+    '''
+
+    beers['nbr_ratings'] = beers['nbr_ratings'].astype(int)
+    beers['avg'] = beers['avg'].astype(float)
+    beers['abv'] = beers['abv'].astype(float)
+    beers['avg_computed']  = beers['avg_computed'].astype(float)
+    beers['zscore'] = beers['zscore'].astype(float)
+    beers['nbr_matched_valid_ratings'] = beers['nbr_matched_valid_ratings'].astype(int)
+    beers['avg_matched_valid_ratings'] = beers['avg_matched_valid_ratings'].astype(float)
+    
+    return beers
+
+    
+def format_attribute_breweries(brewery):
+   
+    '''
+    formatted  attributes of brewerie dataframe
+    '''
+    brewery['nbr_beers']=brewery['nbr_beers'].astype(int)
+   
+    return brewery
+
+def format_attribute_tagged(tagged):
+    '''
+    formatted  attributes of tagged
+    '''
+    tagged['user_id'] = tagged['user_id'].astype(str)
+    tagged['date'] = tagged['date'].astype(int)
+    tagged['date'] = tagged['date'].apply(lambda data_seconds : datetime.fromtimestamp(data_seconds).date())
+
+    return tagged
+
+def format_attribute_users(users):
+
+    
+# Applica la trasformazione solo se il valore non è NaN
+   users['joined'] = users['joined'].apply(lambda data_seconds: datetime.fromtimestamp(data_seconds).date() if pd.notna(data_seconds) else data_seconds)
+   users['user_id'] = users['user_id'].astype(object)
+   
+
+   return users
+
+
+
+
+    
---
+++ src/scripts/data_cleaning/load_file.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+
+def load_dataframes(datapath):
+    # Nomi dei file CSV nelle rispettive cartelle
+    datapath_rate_beer = datapath+'/RateBeer'
+    datapath_beer_advocade = datapath+'/BeerAdvocate'
+    
+    beers_RB_file_name = "beers.csv"  
+    breweries_RB_file_name = "breweries.csv"
+    users_RB_file_name = "users.csv"
+    ratings_RB_file_name = "ratings_RB.csv"
+    tagged_RB_file_name = "reviews_tagged.csv"
+
+    beers_BA_file_name = "beers.csv"  
+    breweries_BA_file_name = "breweries.csv"
+    users_BA_file_name = "users.csv"
+    ratings_BA_file_name = "ratings_BA.csv"
+    ratings_BA_file_name = "ratings_BA.csv"
+    tagged_BA_file_name =  "reviews_tagged.csv"
+  
+
+    beers_RB_path = os.path.join(datapath_rate_beer, beers_RB_file_name)
+    breweries_RB_path = os.path.join(datapath_rate_beer, breweries_RB_file_name)
+    users_RB_path = os.path.join(datapath_rate_beer, users_RB_file_name)
+    rating_RB_path = os.path.join(datapath_rate_beer, ratings_RB_file_name)
+    tagged_RB_path = os.path.join(datapath_rate_beer, tagged_RB_file_name)
+
+    beers_BA_path = os.path.join(datapath_beer_advocade, beers_BA_file_name)
+    breweries_BA_path = os.path.join(datapath_beer_advocade, breweries_BA_file_name)
+    users_BA_path = os.path.join(datapath_beer_advocade, users_BA_file_name)
+    rating_BA_path = os.path.join(datapath_beer_advocade, ratings_BA_file_name)
+    tagged_BA_path = os.path.join(datapath_beer_advocade, tagged_BA_file_name)
+    
+    
+
+    # load csv in dataFrames
+    beer_RB = pd.read_csv(beers_RB_path)
+    breweries_RB = pd.read_csv(breweries_RB_path)
+    users_RB = pd.read_csv(users_RB_path)
+    ratings_RB=pd.read_csv(rating_RB_path)
+    tagged_RB=pd.read_csv(tagged_RB_path)
+
+    beer_BA = pd.read_csv(beers_BA_path)
+    breweries_BA = pd.read_csv(breweries_BA_path)
+    users_BA = pd.read_csv(users_BA_path)
+    ratings_BA =pd.read_csv(rating_BA_path)
+    tagged_BA= pd.read_csv(tagged_BA_path)
+   
+
+    return beer_RB, breweries_RB, users_RB, ratings_RB, tagged_RB, beer_BA, breweries_BA, users_BA, ratings_BA, tagged_BA
