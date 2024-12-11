d3ce85687e823583bc58d7f6b7e2416cd082d269
alessandro-dimaria <alessandro.dimaria@epfl.ch>
2024-12-11 13:39:22 +0100

    Update add_exp.py

--- src/scripts/expert_metric/add_exp.py
+++ src/scripts/expert_metric/add_exp.py
@@ -1,33 +1,14 @@
 import pandas as pd
 import os
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
-from string import punctuation
-import json
+import sys
+sys.path.append('../')
+from helpers import *
 
-def get_exp_stems(expert_terms):
-    expert_terms_stemmed = {}
-    for category, terms in expert_terms.items():
-        tokens = [word_tokenize(term.lower()) for term in terms]
-    
-        stemmer = SnowballStemmer('english')
-        stemmed_tokens = [stemmer.stem(word[0]) for word in tokens]
-        expert_terms_stemmed[category] =  stemmed_tokens
-    
-    return expert_terms_stemmed
-
-def exp_term_score(text_tokens, expert_terms_stemmed):
-    scores = {category: sum(1 for term in text_tokens if term in terms)
-              for category, terms in expert_terms_stemmed.items()}
-    scores["expertness_score"] = sum(scores.values())
-    return scores
-
-def score_df(df, expert_terms_stemmed):
-    scores_df = df["stems"].map(exp_term_score(expert_terms_stemmed)).apply(pd.Series)
-    
-    # Drop intermediate column and merge results
-    return pd.concat([df, scores_df], axis=1)
+def score_df(df, expert_categories, expert_terms_stemmed):
+    for criterion in expert_categories:
+        df[criterion] = df['stems'].apply(lambda cell: len(list(set(cell) & set(expert_terms_stemmed[criterion]))))
+    df['expertness_score'] = df[expert_categories].sum(axis=1)
+    return df
 
 
 def add_exp_scores(data_path):
@@ -47,16 +28,13 @@ def add_exp_scores(data_path):
     - The function applies the `score_df` function to add the stems to each review.
     - The processed DataFrame is saved as 'reviews_w_scores.pkl' in the respective directory.
     """
-    with open( os.path.join(data_path, 'expert_terms.json'), 'r') as f:
-        expert_terms = json.load(f)
-
     advocate_dir = os.path.join(data_path, 'BeerAdvocate')
     stems_ba = pd.read_pickle(os.path.join(advocate_dir, 'reviews_with_exp_stems.pkl'))
 
     rb_dir = os.path.join(data_path, 'RateBeer')
     stems_rb = pd.read_pickle(os.path.join(rb_dir, 'reviews_with_exp_stems.pkl'))
     
-    expert_terms_stemmed = get_exp_stems(expert_terms)
+    expert_terms_stemmed = get_exp_stems(data_path)
 
 
     df_ba = score_df(stems_ba, expert_terms_stemmed) 
